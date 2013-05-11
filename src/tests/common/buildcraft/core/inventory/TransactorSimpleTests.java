2d064b7bdca7eb05af628905d63269839bc6fee1
Krapht <kraphteu@gmail.com>
2013-05-11 13:39:16 +0200

    Make the fake inventory return true on isStackValidForSlot

--- src/tests/common/buildcraft/core/inventory/TransactorSimpleTests.java
+++ src/tests/common/buildcraft/core/inventory/TransactorSimpleTests.java
@@ -1,21 +1,25 @@
 package tests.common.buildcraft.core.inventory;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.anyObject;
-import static org.mockito.Mockito.*;
-import static org.junit.Assert.*;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.ForgeDirection;
 
-import org.junit.*;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import net.minecraft.inventory.IInventory;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraftforge.common.ForgeDirection;
 import buildcraft.core.inventory.TransactorSimple;
-import buildcraft.core.utils.SimpleInventory;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(ItemStack.class)
@@ -53,6 +57,7 @@ public class TransactorSimpleTests {
 	public void inject_inventoryIsEmptyDontAdd_returns64(){
 		when(fakeInventory.getSizeInventory()).thenReturn(1);
 		when(fakeInventory.getStackInSlot(0)).thenReturn(null);
+		when(fakeInventory.isStackValidForSlot(anyInt(), (ItemStack)anyObject())).thenReturn(true);
 		injectStack.stackSize = 64;
 		
 		TransactorSimple cut = new TransactorSimple(fakeInventory);
@@ -65,6 +70,7 @@ public class TransactorSimpleTests {
 	public void inject_inventoryIsEmptyDoAdd_returns64(){
 		when(fakeInventory.getSizeInventory()).thenReturn(1);
 		when(fakeInventory.getStackInSlot(0)).thenReturn(null);
+		when(fakeInventory.isStackValidForSlot(anyInt(), (ItemStack)anyObject())).thenReturn(true);
 		injectStack.stackSize = 64;
 		
 		TransactorSimple cut = new TransactorSimple(fakeInventory);
@@ -88,6 +94,7 @@ public class TransactorSimpleTests {
 	public void inject_inventoryIsEmptyDoAdd_InventoryIsSet(){
 		when(fakeInventory.getSizeInventory()).thenReturn(1);
 		when(fakeInventory.getStackInSlot(0)).thenReturn(null);
+		when(fakeInventory.isStackValidForSlot(anyInt(), (ItemStack)anyObject())).thenReturn(true);
 		injectStack.stackSize = 64;
 		
 		TransactorSimple cut = new TransactorSimple(fakeInventory);
